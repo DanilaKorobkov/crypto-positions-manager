8fb9e0c10800b05c454841dd74a043509d199542
Danila Korobkov <dykorobkov@avito.ru>
2025-08-27 19:54:29 +0300

    Fix active position message (again)

--- internal/domain/services/watcher/service.go
+++ internal/domain/services/watcher/service.go
@@ -115,11 +115,11 @@ func buildOutOfRangeMessage(inactivePosition domain.UniswapV3Position) string {
 
 func buildActivePositionMessage(activePosition domain.UniswapV3Position) string {
 	return fmt.Sprintf(
-		"✅[position](https://app.uniswap.org/positions/v3/base/%s) %s:%s in range: %.2f%%",
+		"✅[position](https://app.uniswap.org/positions/v3/base/%s) %s:%s in range: %d%%",
 		activePosition.ID,
 		activePosition.Token0.Name,
 		activePosition.Token1.Name,
-		calculateTickPercentagePosition(activePosition),
+		int(calculateTickPercentagePosition(activePosition)),
 	)
 }
 
